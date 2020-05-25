55f813ee81de8d36372cb08393a9ec50a29e6826
Peter Stanger <psta191@mywhitecliffe.com>
2020-05-25 19:14:19 +1200

    Initial Commit

---
+++ src/methodoverloadtest/MethodOverloadTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methodoverloadtest;
+
+/**
+ *
+ * @author Peter
+ */
+public class MethodOverloadTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MethodOverload methodOverload = new MethodOverload();
+        methodOverload.testOverloadedMethods();
+    }
+    
+}
